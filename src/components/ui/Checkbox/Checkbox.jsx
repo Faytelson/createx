ec018585a645676588ba08e4ee1666511f4c5e55
Darya <dfaitelson@scid.ru>
2025-08-12 17:25:26 +0300

    CRE-8: Добавить инпуты

--- src/components/ui/Checkbox/Checkbox.jsx
+++ src/components/ui/Checkbox/Checkbox.jsx
@@ -1,16 +1,18 @@
 import styles from "@ui/Checkbox/Checkbox.module.scss";
 import { classNames } from "@utils/utils";
 
-function Checkbox({ checked, label, disabled = false, id, onChange }) {
+function Checkbox({ value, name, checked, label, disabled = false, id, onChange }) {
   const labelClasses = classNames([styles.label, { [styles.label_disabled]: disabled }]);
   return (
     <label className={labelClasses}>
       <input
         className={styles.input}
         type="checkbox"
+        value={value}
+        name={name}
         id={id}
         checked={checked}
-        onChange={(e) => onChange(e.target.checked)}
+        onChange={onChange}
         disabled={disabled}
       />
       {label}
